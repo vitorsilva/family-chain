ad7bae0a7a2d2f66abf6c932bc51250192d7e86b
Vitor Rodrigues <Vitor.Rodrigues@samsys.pt>
2025-10-31 18:55:45 +0000

    chore: update hardhat configuration for improved network settings

--- blockchain/hardhat.config.ts
+++ blockchain/hardhat.config.ts
@@ -36,6 +36,11 @@ const config: HardhatUserConfig = {
       url: configVariable("SEPOLIA_RPC_URL"),
       accounts: [configVariable("SEPOLIA_PRIVATE_KEY")],
     },
+    mainnet: {
+      type: "http",
+      chainType: "l1",
+      url: configVariable("MAINNET_RPC_URL"),
+    },
   },
 };
 
